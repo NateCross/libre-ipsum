a60a4165db6a5d7923e3408cb05a6996e10a5a26
Nathan Angelo B. Cruz <jonathan.a.b.cross@gmail.com>
2022-10-21 15:22:20 +0800

    Made proof of concept, see Upload.tsx

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Inertia\Inertia;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return Storage::download('6fKe2Hcf0sD9dHn7du9IPQuXM3M2T5372C4P1HP3.epub');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Books/Upload', []);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $path = $request->file('book')->store('books');
+        return $path;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Book $book)
+    {
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Book $book)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Book $book)
+    {
+        //
+    }
+}
---
+++ resources/js/Pages/Books/Upload.tsx
@@ -0,0 +1,121 @@
+import React, { FormEvent, ChangeEvent, useState } from 'react'
+import { useForm } from '@inertiajs/inertia-react';
+import epub from 'epubjs';
+import { ReactReader } from 'react-reader';
+
+interface BookUploadForm {
+  book: File | undefined,
+}
+
+export default function Upload() {
+  const { data, setData, post, progress } = useForm<BookUploadForm>({
+    book: undefined,
+  });
+  const [bookInfo, setBookInfo] = useState('');
+  const [location, setLocation] = useState<string | number | undefined>(undefined);
+  const locationChanged = (epubcfi: string) => {
+    setLocation(epubcfi);
+  }
+
+  function handleFileChange(e: ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    
+    // Prevents "Object may possibly be null" error
+    const input = e.target as HTMLInputElement;
+    if (!input.files?.length) return
+
+    setData('book', input.files[0]);
+  }
+
+  // We use an HTTP request to get the image from the url,
+  // then we can load the response into the File Reader to get
+  // a base64 string of the image file that can be saved
+  function getImageFromFileReader(coverUrl: string) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", coverUrl, true);
+    xhr.responseType = "blob";
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== 4) return;
+
+      if (((xhr.status === 200) || (xhr.status == 0)) && (xhr.response)) {
+        const ImgReader = new FileReader();
+        ImgReader.onloadend = function() {
+          // storeBookToLib(bookData.target.result, Lib.bookLib, "Library", metadata, ImgReader.result);
+
+          // Need to execute the functions directly after uploading a book
+          // Async await does not work here, apparently, so we use 'then'
+          // to make these async functions execute one after the other
+          // Lib.saveLibrary().then(() => {
+          //   Lib.openReaderEvent(Lib.bookLib.length - 1)();
+            // Lib.refreshLibraryDisplay();
+          // });
+          // showToast('Added new EPUB to Library.');
+        }
+        ImgReader.readAsDataURL(xhr.response);
+      }
+    }
+    xhr.send(null);
+  }
+
+  function readEpubFile(file: File) {
+    const reader = new FileReader();
+
+    reader.onload = async bookData => {
+      const book = epub({ replacements: 'base64' });
+      const result = bookData.target?.result as string;
+      await book.open(result, 'base64');
+      setBookInfo(result);
+    };
+
+    reader.readAsArrayBuffer(file)
+  }
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if (data?.book) {
+      readEpubFile(data?.book);
+
+      // post('/books');
+    }
+  }
+
+  // let book = epub("books");
+  // let book = epub("https://s3.amazonaws.com/epubjs/books/alice/OPS/package.opf");
+  // let book = epub('x9MnQhZ3hr4CvfRfihF6uT4NCHYRkNARL2L0L2wd.epub');
+  // let rendition = book.renderTo('display', {width: 600, height: 400});
+  // let displayed = rendition.display();
+
+  return (
+    <>
+      <h1
+        className='
+          text-xl
+          text-red-500
+          font-bold
+        '
+      >
+        Upload a Book
+      </h1>
+
+      <form onSubmit={handleSubmit}>
+        <input type="file" onChange={handleFileChange} accept='.epub'  />
+        <button type="submit">Submit</button>
+      </form>
+
+      <div>
+
+      </div>
+      {bookInfo && <div className='h-screen'>
+        <ReactReader 
+          location={location}
+          locationChanged={locationChanged}
+          url={bookInfo}
+        />
+      </div>}
+
+
+
+    </>
+  )
+}
